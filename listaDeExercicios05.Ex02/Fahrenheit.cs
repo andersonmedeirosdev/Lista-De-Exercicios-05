55c93d0a4747acb25faf9842039d115d4551045d
Anderson Medeiros <andersonviniciusmedeiros97@gmail.com>
2023-04-05 11:54:50 -0300

    Finalizando exercício 02.

---
+++ listaDeExercicios05.Ex02/Fahrenheit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace listaDeExercicios05.Ex02
+{
+    internal class Fahrenheit
+    {
+        internal double fahrenheit;
+
+        public double CalcularCelsius()
+        {
+            return (fahrenheit - 32) / 1.8;
+        }
+    }
+}
---
+++ listaDeExercicios05.Ex02/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace listaDeExercicios05.Ex02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fahrenheit temperatura = new Fahrenheit();
+
+            temperatura.fahrenheit = 110;
+
+            Console.Write("A temperatura em Farenheit é: " + temperatura.CalcularCelsius() );
+        }
+    }
+}
