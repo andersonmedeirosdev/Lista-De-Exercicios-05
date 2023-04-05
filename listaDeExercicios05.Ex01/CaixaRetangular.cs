3d657201c8fc6f8edbb0ea806729827e18e7436f
Anderson Medeiros <andersonviniciusmedeiros97@gmail.com>
2023-04-05 11:28:25 -0300

    Add project files.

---
+++ listaDeExercicios05.Ex01/CaixaRetangular.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace listaDeExercicios05.Ex01
+{
+    internal class CaixaRetangular
+    {
+        internal int altura;
+        internal int comprimento;
+        internal int largura;
+
+        public decimal CalcularVolume()
+        {
+            return altura * comprimento * largura;
+        }
+    }
+}
---
+++ listaDeExercicios05.Ex01/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace listaDeExercicios05.Ex01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CaixaRetangular caixa = new CaixaRetangular();
+            caixa.altura = 10;
+            caixa.comprimento = 20;
+            caixa.largura = 30;
+            Console.Write("O volume da caixa é: " + caixa.CalcularVolume() );
+        }
+    }
+}
