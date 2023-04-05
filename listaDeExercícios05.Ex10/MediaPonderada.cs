ff9153625853786b084d9edeb4410c483db35a40
Anderson Medeiros <andersonviniciusmedeiros97@gmail.com>
2023-04-05 15:17:37 -0300

    Finalizando ultimo exercício da Lista.

---
+++ listaDeExercícios05.Ex10/MediaPonderada.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace listaDeExercícios05.Ex10
+{
+    internal class MediaPonderada
+    {
+        internal double nota1 = 10, nota2 = 8, nota3 = 7, nota4 = 6, mediaP, calculo1, calculo2;
+        internal double peso1 = 1, peso2 = 2, peso3 = 3, peso4 = 4;
+
+        public double CalcularMedia()
+        {
+            calculo1 = nota1 * peso1 + nota2 * peso2 + nota3 * peso3 + nota4 * peso4;
+            calculo2 = peso1 + peso2 + peso3 + peso4;
+            mediaP = calculo1 / calculo2;
+            return mediaP;
+        }
+    }
+}
---
+++ listaDeExercícios05.Ex10/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace listaDeExercícios05.Ex10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MediaPonderada media = new MediaPonderada();
+
+            Console.WriteLine("A média ponderada é: " + media.CalcularMedia() );
+        }
+    }
+}
