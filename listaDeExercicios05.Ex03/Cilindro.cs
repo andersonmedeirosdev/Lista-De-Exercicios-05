013e53573de2384423b2c576df18b00542d0bb50
Anderson Medeiros <andersonviniciusmedeiros97@gmail.com>
2023-04-05 13:16:57 -0300

    Finalizando exercício 03.

---
+++ listaDeExercicios05.Ex03/Cilindro.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace listaDeExercicios05.Ex03
+{
+    internal class Cilindro
+    {
+        internal int raio;
+        internal int altura;
+
+        public decimal CalcularVolume()
+        {
+            return 3.14M * (raio * raio) * altura;
+        }
+    }
+}
---
+++ listaDeExercicios05.Ex03/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Http.Headers;
+
+namespace listaDeExercicios05.Ex03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cilindro medida = new Cilindro();
+
+            medida.raio = 10;
+            medida.altura = 80;
+            Console.Write("O volume do cilindro é: " + medida.CalcularVolume() );
+        }
+    }
+}
