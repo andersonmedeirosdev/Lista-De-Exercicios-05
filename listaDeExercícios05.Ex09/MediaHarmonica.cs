aa6aacd97911f0c8c5bd47b875fc7ad695fe3690
Anderson Medeiros <andersonviniciusmedeiros97@gmail.com>
2023-04-05 15:00:07 -0300

    Finalizando Ex 09.

---
+++ listaDeExercícios05.Ex09/MediaHarmonica.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace listaDeExercícios05.Ex09
+{
+    internal class MediaHarmonica
+    {
+        internal double nota1 = 8, nota2 = 7, nota3 = 6, nota4 = 9, mediaH;
+
+        public double CalcularMedia() 
+        {
+            mediaH = 4 / (1/nota1 + 1/nota2 + 1/nota3 + 1/nota4);
+            return Math.Round(mediaH, 2);
+        }
+    }
+}
---
+++ listaDeExercícios05.Ex09/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace listaDeExercícios05.Ex09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MediaHarmonica media = new MediaHarmonica();
+
+            Console.WriteLine("A média harmônica é: " + media.CalcularMedia() );
+            Console.ReadLine();
+        }
+    }
+}
