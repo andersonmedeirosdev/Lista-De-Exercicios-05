08075cc4933b09cf698a3bee04b6eae980cfd637
Anderson Medeiros <andersonviniciusmedeiros97@gmail.com>
2023-04-05 13:56:42 -0300

    Finalizando exercício 04.

---
+++ listaDeExercicios05.ex04/Consumo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace listaDeExercicios05.ex04
+{
+    internal class Consumo
+    {
+        internal double kminicial = 10000;
+        internal double kmfinal = 15000;
+        internal double kmtotal;
+        internal double litrosgastos;
+        internal double consumototal;
+
+        public double CalcularConsumo()
+        {
+            kmtotal = kmfinal - kminicial;
+            litrosgastos = 120;
+            consumototal = (kmtotal / litrosgastos);
+            return Math.Round(consumototal, 2);
+        }
+    }
+}
---
+++ listaDeExercicios05.ex04/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace listaDeExercicios05.ex04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Consumo veiculo = new Consumo();
+
+            Console.Write("O consumo por Km é: " + veiculo.CalcularConsumo() );
+            Console.ReadLine();
+        }
+    }
+}
